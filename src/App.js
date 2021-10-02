0c60c3c8a3c0079b5e55ca7ff0fd1ef039808285
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-10-02 19:50:46 +0530

    basic scss added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import "./sass/main.scss";
+
+//* Components
+import Loader from './components/Loader';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+      ? document.querySelector('body').classList.add('loading')
+      : document.querySelector('body').classList.remove('loading');
+  }, [loading]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {loading ? (
+      <Loader setLoading={setLoading}/>
+    ) : (
+      <>
+      </>
+    )}
+    </>
   );
 }
 
